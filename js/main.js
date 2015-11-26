5fe89ff8df8dc71ee88cdfb319f5a334f524279c
Graphic <graphic@SOD-SJH-518-03M.local>
2015-11-26 15:37:19 -0500

    multiple attempts to display random images, difficulty determining how to set variables

--- js/main.js
+++ js/main.js
@@ -1,9 +1,57 @@
-var random_images_array = ['../img/cat1.png', '../img/cat2.png', '../img/cat3.png', '../img/cat4.png'];
+// This coding was to display one random image at refresh
 
-function getRandomImage(imgAr, path) {
-	path = path || 'images/';
-	var num = Math.floor( Math.random() * imgAr.length);
-	var img = imgAr[ num ];
-	var imgStr = '<img src="' + path + img + '" alt = ""';
-	document.write(imgStr); document.close();
+// var random_images_array = ['../img/cat1.png', '../img/cat2.png', '../img/cat3.png', '../img/cat4.png'];
+
+
+// function getRandomImage(imgAr, path) {
+// 	path = path || 'images/';
+// 	var num = Math.floor( Math.random() * imgAr.length);
+// 	var img = imgAr[ num ];
+// 	var imgStr = '<img src="' + path + img + '" alt = ""';
+// 	document.write(imgStr); document.close();
+// }
+
+
+//ask if each category of images should be made (cat, dog, other)
+
+var catImage = [
+
+'../img/cat1.png',
+
+'../img/cat2.png',
+
+'../img/cat3.png',
+
+'../img/cat4.png',
+
+];
+
+
+
+// var levels = [     ]
+
+
+
+function updateRandomImages() {
+	var imageElements = $('img');
+	$.each(imageElements, function(index, element) {
+		displayRandomImage(element);
+	});
 }
+
+function displayRandomImage(element) {
+    var index = getRandomNumber(); 
+    $(element).attr( 'src', imageList[index] );
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 10);
+}
+
+$('button').click(function () {
+    updateRandomImages();
+});
+s
+//start off with some random images
+updateRandomImages();
+
